60edd89afbbe93152f582727294c528dd83fd967
Célyan Mellinger <celyan.mellinger@hotmail.fr>
2026-02-10 09:28:29 +0100

    db and gitignore

--- src/shared/database/schema.sql
+++ src/shared/database/schema.sql
@@ -3,6 +3,9 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email VARCHAR(255) UNIQUE NOT NULL,
   password_hash VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  pseudo VARCHAR(255) UNIQUE NOT NULL,
   created_at TIMESTAMP DEFAULT NOW(),
   updated_at TIMESTAMP DEFAULT NOW()
 );
